eb6961fdf7c734ed32d4156ccc0adbec5a64c92a
Phoenix Perry <contact@phoenixperry.com>
2020-06-16 16:51:28 +0100

    adding basic code

---
+++ Basic_coding/Assets/Basic_Coding.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//this is a class in Unity. It is called Basic_Coding, which is the name of our script. MonoBehavior is it's parent. All you need to know is it means you can use the Unity Engine for now. Search dotnetpearls + inheritance after this class if you're really curious!
+public class Basic_Coding : MonoBehaviour
+{
+    //comments are super useful! The engine will ignore any line with // at the start. You could write a whole novel here and it'd ignore you! 
+
+    // VARIABLES:
+    int percentageCaffinated; // The integer variable "percentageCaffinated" holds a whole number 
+
+    // Start is called before the first frame update and only once
+    void Start()
+    {
+        percentageCaffinated = 0; // Let's save the number 0 because there's no coffee in our body yet. Let's change that STAT! 
+
+        wakeUp(); //wakey wakey sleephead
+    }
+
+    void wakeUp() {
+        //FOR LOOPS: for as long as a condition is true, do a thing. Until you are awake, drink some coffee! 
+        for (int i = 0; i < 101; i++)
+        { // For each of the numbers 0 through 100
+            percentageCaffinated = i;
+            //add one to i each time through the loop. 
+        }
+    }
+
+    //Unity runs update every frame, usually about 1/60th of a second.
+    void Update()
+    {
+        //because no one should do math homework tired.... 
+        if (percentageCaffinated == 100) //a double == checks to see if two values are the same :D 
+        {
+            int answer = doYourMathHomework(13, 23);
+           // Debug.Log(answer);  (Uncomment to see the answer!) 
+        }
+    }
+
+    //FUNCTIONS:
+    int doYourMathHomework(int a, int b)
+    {
+        int sum = a + b; // The answer is the sum of the two numbers.
+        return sum; // Returns the sum of a+b to the location in the code that called it, in this case, line 36. 
+    }
+
+  }
---
+++ Basic_coding/Assets/Pet.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pet : MonoBehaviour
+{
+    //this is a class called Pet 
+    int fullness = 0;
+    bool asleep = false; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("click on the game window in Unity to select it"); 
+        Debug.Log("Press f to feed");
+        Debug.Log("Press c to cuddle"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.F)) //This is how unity checks for key input. Youc can put any letter on your keyboard here 
+        {
+            feed(); //call the feed function. Feed that critter! 
+        }
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            cuddle(); //pet that cat!
+        }
+
+
+        if (fullness > 10 && fullness < 20) //if statements are really cool! You can stack conditions together just using &&
+        {
+            Debug.Log("This is good! Meow! Soon, I will fall sleep happy in the sun."); 
+        }
+
+        if (fullness > 20) {
+            asleep = true; 
+            Debug.Log("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"); 
+        }
+ 
+    }
+
+    void feed()
+    {
+        if (asleep == false)
+        {
+            fullness = fullness + 1; //food is good and makes you full! add 1 to fullness 
+            Debug.Log("Nom, nom, nom");
+        }
+        
+    }
+
+    //hot tip! You can't name a function the same as your class. That's a reserved function called a constructor. Google dotnetperals and constructor to learn more but you don't need it in this class. 
+    void cuddle() {
+        Debug.Log("Pur, pur"); 
+    }
+
+}
